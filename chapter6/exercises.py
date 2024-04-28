5264a4a710c531a74dc4d89b5127c1318c90314d
Andrew <andrew.colinet@alum.utoronto.ca>
2024-04-27 23:34:33 -0400

    Cleaned up answers to questions for readability.

--- chapter6/exercises.py
+++ chapter6/exercises.py
@@ -1,41 +1,70 @@
 
 
 
-# 1.
+# Q1: What is the approximate depth of a decision tree trained (without
+#     restrictions) on a training set with one million instances?
+
 print(f"""Since the decision tree was trained without restrictions then
 the approximate depth of the tree would be log_2(10^6) since we
 could repeatedly halve the group until all instances are of the
 same type.\n""")
 
 
-# 2.
+# Q2: Is a node's Gini impurity generally lower or higher than its parent's? Is
+#     it generally lower/higher, or always lower/higher?
+
 print(f"""A node's Gini impurity is generally lower than its parent's Gini
 impurity. It is generally lower but can be equal or greater.\n""")
 
 
-# 3.
+# Q3: If a decision tree is overfitting the training set, is it a good idea to try
+#     decreasing max_depth?
+
 print(f"""Yes, if the decision tree is overfitting then decreasing max
 depth will regularize the decision tree.\n""")
 
 
-# 4.
+# Q4: If a decision tree is underitting the training set, is it a good idea to try
+#     scaling the input features.
+
 print(f"""No, decision trees do not depend on scaling input features.
 If it is underfitting the training set this will not help.\n""")
 
 
-# 5.
+# Q5: If it takes one hour to train a decision tree on a training set containing
+#     one million instances, roughly how much time will it take to train
+#     another decision tree on a training set containing ten million instances?
+#     Hint: consider the CART algorithm's computational complexity.
+
 print(f"""We are given that O(n(10^6)log_2(10^6)) ~ 1 hour. Thus,
 O(n(10^7)log_2(10^7)) = 10*O(n(10^6)log_2(10*10^6)) =
 10*O(n(10^6)log_2(10^6)) + 10(1/6)*O(n(10^6)log_2(10^6)) ~
 10 hours + 10(1/6) hours = 11.6666666667 hours.\n""")
 
 
-# 6.
+# Q6: If it takes one hour to train a decision tree on a given training set,
+#     roughly how much time will it take if you double the number of
+#     features?
+
 print(f"""We are given that O(nmlog_2(m)) ~ 1 hour. If we double n then
 O(2nmlog_2(m)) = 2*O(nmlog_2(m)) ~ 2 hours.\n""")
 
 
-# 7.
+# Q7: Train and fine-tune a decision tree for the moons dataset by following
+#     these steps:
+#
+#     a. Use make_moons(n_samples=10000, noise=0.4) to generate a
+#        moons dataset.
+#     b. Use train_test_split() to split the dataset into a training set and a test
+#        set.
+#     c. Use grid search with cross-validation (with the help of the
+#        GridSearchCV class) to find good hyperparameter values for a
+#        DecisionTreeClassifier. Hint: try various values for
+#        max_leaf_nodes.
+#     d. Train it on the full training set using these hyperparameters, and
+#        measure your model's performance on the test set. You should get
+#        roughly 85% to 87% accuracy.
+
 from sklearn.datasets import make_moons
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
@@ -81,7 +110,24 @@ print(f"accuracy function: {100 * accuracy_score(y_moons_test, pred, normalize=T
 
 
 
-# 8.
+# Q8: Grow a forest by following these steps:
+#
+#     a. Continuing the previous exercise, generate 1,000 subsets of the
+#        training set, each containing 100 instances selected randomly. Hint:
+#        you can use Scikit-Learn's ShuffleSplit class for this.
+#     b. Train one decision tree on each subset, using the best
+#        hyperparameter values found on the test set. Since they were trained on
+#        smaller sets, these decision trees will likely perform worse than the
+#        first decision tree, achieving only about 80% accuracy.
+#     c. Now comes the magic. For each test set instance, generate the
+#        predictions of the 1,000 decision trees, and keep only the most
+#        frequent prediction (you can use SciPy's model() function for this).
+#        This approach gives you majority-vote predictions over the test set.
+#     d. Evaluate these predictions on the test set: you should obtain a
+#        slightly higher accuracy than your first model (about 0.5 to 1.5%
+#        higher). Congratulations, you have trained a random forest
+#        classifier!
+
 from sklearn.model_selection import ShuffleSplit
 
 # a., b.
